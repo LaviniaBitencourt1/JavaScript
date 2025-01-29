428ac34a576cd32557645a994e0412022a453df4
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-01-29 09:58:16 -0300

    atualização

--- Exercícios/lista-1.js
+++ Exercícios/lista-1.js
@@ -1,3 +1,4 @@
+console.log("Exercício 1")
 
 let a = 1
 let b = 4
@@ -17,6 +18,8 @@ console.log (div)
 let resto = a % b
 console.log (resto)
 
+console.log("-----------------")
+console.log("Exercício 2")
 
 let altura = 10
 let largura = 5
@@ -24,6 +27,8 @@ let largura = 5
 let area = altura * largura
 console.log (area)
 
+console.log("-----------------")
+console.log("Exercício 3")
 
 let c = 5
 
@@ -33,6 +38,8 @@ console.log (dobro)
 let metade = c / 2
 console.log (metade)
 
+console.log("-----------------")
+console.log("Exercício 4")
 
 let n1 = 7
 let n2 = 5
@@ -41,6 +48,8 @@ let n3 = 8
 let media = (n1 + n2 + n3) / 3
 console.log (media)
 
+console.log("-----------------")
+console.log("Exercício 5")
 
 let celsius = 30
 
