700132769488493f21d80c93732c8742756b0105
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-01-29 11:06:58 -0300

    nova lista

---
+++ Exercícios/lista-2.js
@@ -0,0 +1,62 @@
+console.log("Exercício 1")
+
+let a = 10
+let b = 15
+
+let maior = a > b
+console.log(maior)
+
+let menor = a <= b
+console.log(menor)
+
+let igual = a == b
+console.log(igual)
+
+console.log("-----------------")
+console.log("Exercício 2")
+
+let MenorIdade = 15
+let MaiorIdade = 19
+
+let OpcionalVotar = MenorIdade >= 16
+let ObrigadoVotar = MaiorIdade >= 18
+console.log(OpcionalVotar)
+console.log(ObrigadoVotar)
+
+console.log("-----------------")
+console.log("Exercício 3")
+
+let idade = 90
+
+let MaiordeIdade = idade >=18 
+let idoso =  idade >= 60
+console.log (MaiordeIdade)
+console.log (idoso)
+
+console.log("-----------------")
+console.log("Exercício 4")
+
+let usuario = "aluno"
+console.log (usuario == "admin")
+
+console.log("-----------------")
+console.log("Exercício 5")
+
+let num1 = 6
+let num2 = 10
+
+let divisivelTres = num1 % 3
+let divisivelCinco = num2 % 5
+console.log(divisivelTres)
+console.log(divisivelCinco)
+
+console.log("-----------------")
+console.log("Exercício 6")
+
+let saldoConta = 1000
+let valorSaque = 105
+
+let saldoSuficiente = valorSaque < saldoConta
+let saldoNaoSuficiente = saldoConta - valorSaque > 100 
+console.log(saldoSuficiente)
+console.log(saldoNaoSuficiente)
