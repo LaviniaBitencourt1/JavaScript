69f32a9a96bb9b46a87c88079f1f8ac4b408b04c
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-01-29 08:19:37 -0300

    Initial commit

---
+++ Exercícios/lista-1.js
@@ -0,0 +1,53 @@
+//Exercício 1
+
+let a = 1
+let b = 4
+
+let soma = a + b
+console.log (soma)
+
+let sub = a - b
+console.log (sub)
+
+let mult = a * b
+console.log (mult)
+
+let div = a / b
+console.log (div)
+
+let resto = a % b
+console.log (resto)
+
+//Exercício 2
+
+let altura = 10
+let largura = 5
+
+let area = altura * largura
+console.log (area)
+
+//Exercício 3
+
+let c = 5
+
+let dobro = c * 2
+console.log (dobro)
+
+let metade = c / 2
+console.log (metade)
+
+//Exercício 4
+
+let n1 = 7
+let n2 = 5
+let n3 = 8
+
+let media = (n1 + n2 + n3) / 3
+console.log (media)
+
+//Exercício 5
+
+let celsius = 30
+
+let fahrenheit = (celsius * 9/5) + 32
+console.log (fahrenheit)
---
+++ Operadores/aritmeticos.js
@@ -0,0 +1,30 @@
+//console.log ("Olá, Mundo!")
+
+//para delarar variáveis
+let nome = "Lavinia" //vamos usar mais esse
+var nomes = "Lavinia"
+
+//  CTRL + ; -> COMENTÁRIOS
+console.log(nome)
+
+let idade = 16
+console.log("Minha idade é", idade, "anos")
+//CONCATENAÇÃO = juntar texto e valor
+
+//OPERAÇÕES ARITMÉTICAS
+let a = 3
+let b = 2
+
+let soma =  a + b
+let sub = a - b
+let mult = a * b
+let div = a / b
+let resto = a % b// o que sobra da divisão
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+console.log(resto)
+
+
