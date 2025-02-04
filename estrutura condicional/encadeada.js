dfb7f2aabd6d7570f1b823ce7f4ab9ee66884897
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-02-04 10:16:33 -0300

    Create encadeada.js

---
+++ estrutura condicional/encadeada.js
@@ -0,0 +1,9 @@
+//Numero positivo, numero negativo ou zero
+let numero = prompt("Escola um número")
+if (numero > 0 ){
+    console.log("Positivo")
+} else if (numero < 0) {
+    console.log("Negativo")
+} else {
+    console.log("Zero")
+}
