152676aade96d9f7e40b55b5e8d52d2a99e3366e
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-02-05 13:03:57 -0300

    Lista 6 criada

---
+++ Exercícios/lista-6.js
@@ -0,0 +1,39 @@
+console.log("Exercício 1")
+let opcao = (prompt("Digite um número entre 1 e 7:"))
+
+switch (opcao){
+    case "1":
+        console.log("Domingo")
+        break
+    case "2":
+        console.log("Segunda-Feira")
+        break
+    case "3":
+        console.log("Terça-Feira")
+        break
+    case "4":
+        console.log("Quarta-Feira")
+        break
+    case "5":
+        console.log("Quinta-Feira")
+        break
+    case "6":
+        console.log("Sexta-Feira")
+        break
+    case "7":
+        console.log("Sábado-Feira")
+        break
+        default:
+        console.log("Número inválido. Inssira um valor entre 1 e 7;")
+}
+
+console.log("------------------")
+console.log("Exercício 2")
+let idade = (prompt("Digite sua idade:"))
+
+switch (idade){
+    case "5":
+        console.log("Infantil A")
+        break
+       
+    }
