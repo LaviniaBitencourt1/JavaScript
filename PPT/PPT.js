600cbf50a0ebc9bf0960c72fc08b9fe417ceb5fd
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-02-25 09:10:15 -0300

    atualização PPT

--- PPT/PPT.js
+++ PPT/PPT.js
@@ -1,27 +1,48 @@
 //Jogo Pedra, Papel e Tesoura
-const escolhaJogador = prompt ("Escolha pedra, papel ou tesoura:")
-//Solicita que o usuário informe qual ele quer
+let placarJogador = 0
+let placarPC = 0
 
-const escolhaPC = ["pedra", "papel", "tesoura"][Math.floor(Math.random() * 3)]
-//Gera uma escolha aleatoria para o computador
-//Criamos um array com as opções e usamos o random para escolher uma de forma aletória
+//Variavel para armazenar as rodadas
+let rodadas = Number(prompt("Quantas vezes deseja jogar?"))
 
-alert (`Você escolheu ${(escolhaJogador)}`)
-alert (`O computador escolheu ${(escolhaPC)}`)
-//Mostrando qual foi a escolha do computador
+//Laço de repetição que fará as rodadas
+for (let i = 0; i < rodadas; i++) {
 
-//Compara se a nossa escolha foi igual a do computador
-if (escolhaJogador == escolhaPC) {
-    alert("Empate")
-} else if (escolhaJogador == "pedra" && escolhaPC == "tesoura"){
-    //criamos a primeira verificada de vitoria
-    alert("Você venceu")
-} else if (escolhaJogador == "tesoura" && escolhaPC == "papel"){
-    //criamos a segunda verificação
-    alert ("Você venceu")
-} else if (escolhaJogador == "papel" && escolhaPC == "pedra"){
-    //criamos a terceira verificação
-    alert("Você venceu")
-} else {
-    alert ("Você perdeu")
+
+
+    const escolhaJogador = prompt("Escolha pedra, papel ou tesoura:")
+    //Solicita que o usuário informe qual ele quer
+
+    const escolhaPC = ["pedra", "papel", "tesoura"][Math.floor(Math.random() * 3)]
+    //Gera uma escolha aleatoria para o computador
+    //Criamos um array com as opções e usamos o random para escolher uma de forma aletória
+
+    alert(`Você escolheu ${(escolhaJogador)}`)
+    alert(`O computador escolheu ${(escolhaPC)}`)
+    //Mostrando qual foi a escolha do computador
+
+    //Compara se a nossa escolha foi igual a do computador
+    if (escolhaJogador == escolhaPC) {
+        alert("Empate")
+    } else if (escolhaJogador == "pedra" && escolhaPC == "tesoura") {
+        //criamos a primeira verificada de vitoria
+        alert("Você venceu")
+        placarJogador++
+    } else if (escolhaJogador == "tesoura" && escolhaPC == "papel") {
+        //criamos a segunda verificação
+        alert("Você venceu")
+        placarJogador++
+    } else if (escolhaJogador == "papel" && escolhaPC == "pedra") {
+        //criamos a terceira verificação
+        alert("Você venceu")
+        placarJogador++
+    } else {
+        alert("Você perdeu")
+        placarPC++
+    }
+    //Exibe o placar no console
+    console.log("Placar Jogador: ", placarJogador)
+    console.log("Placar Computador: ", placarPC)
 }
+//Placar Final
+alert(`Fim do jogo! Plcar final: Jogador ${placarjogador} x ${placarPC} Computador`)
