985ab5b967d08effd20a7bcf98317ec655600ef7
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-04-09 09:19:07 -0300

    criação de pastas novas

---
+++ Data/main.js
@@ -0,0 +1,11 @@
+let dataAtual = new Date()
+console.log(dataAtual)
+
+let dia = diaAtual.getDate()
+let mes = dataAtual.getMonth()
+let ano = dataAtual.getFullYear()
+console.log(`${dia}/${mes}/${ano}`)
+
+let horas = dataAtual.getHours()
+let minutos = dataAtual.getMinutes()
+console.log(`${horas}: ${minutos}`)
---
+++ setInterval/setInterval.js
@@ -0,0 +1,11 @@
+let contato = 0
+
+setInterval(function(){
+    contador++
+    console.log("Contador: " +contador)
+}, 1000)
+function pararContador (){
+    clearInterval(intervalo)
+}
+
+iniciarContador()
