f35dff05c0a252e41e18517c67798fbb4b28c3fd
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-04-22 11:32:24 -0300

    Pasta nova

--- LocalStorage/script.js
+++ LocalStorage/script.js
@@ -1,5 +1,10 @@
 //QUANDO CARREGAR A PÁGINA, MOSTRAR O QUE JÁ ESTAVA SALVO
-
+window.onload = function(){
+    const textoSalvo = localStorage.getItem("minhaAnotacao")
+    if(textoSalvo){
+        document.getElementById("anotacao").value = textoSalvo
+    }
+}
 //SALVAR QUANDO CLICAR NO BOTÃO
 function salvarAnotacao(){
     const conteudo = document.getElementById("anotacao").value
