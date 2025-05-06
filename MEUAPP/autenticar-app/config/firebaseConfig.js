e29b79b5068d24ba3bed1fffff487f5ce830d61e
LaviniaBitencourt1 <lavinia.bitencourt@aluno.senai.br>
2025-05-06 16:58:25 -0300

    criacao do app

---
+++ MEUAPP/autenticar-app/config/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+const configuracoesFirebase = {
+    apiKey: "AIzaSyB2jNawT7uC4qvDf1_wmSh5I03nR1vReGY",
+    authDomain: "tds2-2025-autenticar. firebaseapp.com",
+    projectId: "tds2-2025-autenticar",
+    storageBucket: "tds2-2025-autenticar.firebasestorage.app",
+    messagingSenderId: "510050860669",
+    appId: "1:510050860669:web: 051224d9ebda2482e23b14",
+    measurementId: "G-OVTZLKOOP8"
+    };
+
+const app = initializeApp(configuracoesFirebase);
+export const autenticacao = getAuth(app);
